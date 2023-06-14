07fffb8e9256576356da3825857d6f8f98b9cc07
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-14 15:16:47 -0300

    Api useState

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 
-export default function App() {
-  return (
+export default function App(){
+
+  const [nome, setNome] = useState('Ricardo');
+
+  const [input, setInput] = useState('');
+
+  function alteraNome(){
+    setNome(input);
+    setInput('')
+  }
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput
+      placeholder="Seu nome..."
+      value={input}
+      onChangeText={(texto) => setInput(texto)}
+      />
+      <TouchableOpacity style={styles.btn} onPress={alteraNome}>
+        <Text style={styles.btnText}>Altera Nome</Text>
+      </TouchableOpacity>
+      <Text style={styles.texto}>{nome}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 15
   },
-});
+  texto:{
+    color: '#000',
+    fontSize: 35
+  },
+  bnt:{
+    color: '#222',
+    alignItems: 'center'
+  },
+  btnText:{
+    color: '#fff'
+  }
+})
